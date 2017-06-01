239c7f7ec01f373a94244d1a28c9d52ad9fff29c
wangzhen <zhen.wang@dianrong.com>
2017-06-01 09:35:49 +0800

    first commit

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+/**
+ * Created by esta on 2017/5/31.
+ */
+'use strict';
+
+module.exports = function (grunt) {
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+
+    });
+};
